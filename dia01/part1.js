561c4cd0570c26b191c9bc3a46a1747661918197
Marcelo Hippolyto de Sandes Peixoto <marcelo052001@gmail.com>
2025-12-02 14:20:52 +0000

    Dia 01 Parte 1 Concluida

--- dia01/part1.js
+++ dia01/part1.js
@@ -1,18 +1,42 @@
 const fs = require('fs');
 
 // 1. LENDO O ARQUIVO
-// O 'utf-8' é para ele entender acentos e texto normal
-// O 'trim()' remove aquele espaço em branco vazio que sobra na última linha
 const rawInput = fs.readFileSync('./input.txt', 'utf-8').trim();
 
 // 2. TRANSFORMANDO EM LISTA
-// O split('\n') "corta" o texto toda vez que acha uma quebra de linha
 const lines = rawInput.split('\n');
 
-// Só para você conferir se leu certo:
-console.log('Total de linhas lidas:', lines.length);
-console.log('Primeira linha:', lines[0]);
+// LÓGICA DO PROBLEMA
+let posicaoAtual = 50; // O enunciado diz que começa no 50
+let contadorZeros = 0; // Quantas vezes parou no 0
+
+// Passando linha por linha
+for (let i = 0; i < lines.length; i++) {
+    const linha = lines[i];
+    
+    // 1. Separar a Letra do Número
+    const direcao = linha[0]; // Pega a primeira letra ('R' ou 'L')
+    const valor = parseInt(linha.substring(1)); // Pega do 2º caractere até o fim e vira número
+    
+    // 2. Aplicar a Rotação
+    if (direcao === 'R') {
+        // Direita = Soma
+        posicaoAtual = (posicaoAtual + valor) % 100;
+    } else if (direcao === 'L') {
+        // Esquerda = Subtrai
+        posicaoAtual = (posicaoAtual - valor) % 100;
+        
+        // Transformar negativo no positivo equivalente
+        if (posicaoAtual < 0) {
+            posicaoAtual += 100;
+        }
+    }
+
+    // 3. Verificar se caiu no zero
+    if (posicaoAtual === 0) {
+        contadorZeros++;
+    }
+}
+
 console.log('-------------------');
-
-// --- SUA LÓGICA COMEÇA AQUI ---
-
+console.log('RESPOSTA FINAL:', contadorZeros);
